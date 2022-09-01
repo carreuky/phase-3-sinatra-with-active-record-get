85db66bb944b6bf31d1b5b9f1db619444ad61b0d
carreuky <carreuky@gmail.com>
2022-09-01 09:01:34 +0300

    commit tree 4dd99c484d0060d3a956abec7d8ab05ee0d63423

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+
+    ##{ message: "Hello world" }.to_json
+    games=Game.all.order(:title).limit(25)
+    games.to_json
+
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
